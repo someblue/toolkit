82b1548dbad95fd77c31ef7646e5cbdc9e739214
suzhuorong <suzhuorong@bytedance.com>
2020-12-24 17:31:16 +0800

    refactor: strgen use crypto/rand instead of math/rand
    Change-Id: I397a636c10ec929f5a54d5d0b61d62bcab985b92

--- strgen/main.go
+++ strgen/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 18; i++ {
-		idx := rand.Intn(len(charset))
+	bs := make([]byte, 16)
+	_, err := rand.Read(bs)
+	if err != nil {
+		panic(err)
+	}
+	for _, b := range bs {
+		idx := int(b) % len(charset)
 		fmt.Printf("%c", charset[idx])
 	}
 	fmt.Println("")
